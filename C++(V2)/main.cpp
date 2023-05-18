7fdd8a3bb07125e450fc896b815dcdab14143ae5
HalilSacirovic <halilsacirovic03@gmail.com>
2023-05-18 15:43:27 +0200

    Test???

--- C++(V2)/main.cpp
+++ C++(V2)/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 /*
 	Realizovati klasu Tacka sa privatnim atributima x i y.
 	Metode: inicijalizacija(a,b) koja postavlja vrednosti atributa na vrednosti koje dolazi kao argumenti, translacija(int dx,int dy) koja translira tacku za vrednosti dx po x, odnosno dy po y osi;
-	pozicija() ispisuje trenutnu poziciju tacke u koordinatnom sistemu (njene x i y koordinate) 
+	pozicija() ispisuje trenutnu poziciju tacke u koordinatnom sistemu (njene x i y koordinate)
 */
 class Tacka
 {
@@ -15,7 +15,7 @@ public:
 	void Inic(int a,int b);
 	void Trans(int dx, int dy);
 	void pozicija();
-	
+
 }tacka1;
 
 void Tacka::Inic(int a ,int b ){
@@ -31,19 +31,19 @@ void Tacka::Trans(int dx, int dy){
 void Tacka::pozicija(){
 	cout<<"(x,y)"<<x<<","<<y<<endl;
 }
-void main(){
+int  main(){
 
-Tacka t2;
-tacka1.Inic(1,20);
-tacka2.Inic(5,5);
+Tacka t1,t2;
+t1.Inic(1,20);
+t2.Inic(5,5);
 cout<<"Tacka2";
-tacka1.pozicija();
+t1.pozicija();
 
 cout<<"Tacka1";
-tacka2.pozicija();
-tacka2.Trans(3,5);
+t2.pozicija();
+t2.Trans(3,5);
 
 cout<<"Tacka2";
-tacka2.pozicija();
+t2.pozicija();
 return 0;
 }
---
+++ Zadaci sa Klasama/Untitled1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    cout << "Zašto nece?" << endl;
+    return 0;
+}
+
