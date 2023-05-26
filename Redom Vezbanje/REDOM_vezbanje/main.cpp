9524be94a966abd95ed476406ed2db13973d05d8
HalilSacirovic <halilsacirovic03@gmail.com>
2023-05-26 12:08:44 +0200

    KLASE OVDE

---
+++ Redom Vezbanje/REDOM_vezbanje/main.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <math.h>
+#include <string.h>
+
+using namespace std;
+
+
+
+// KLASE ZADACIIIIIIIIIIIIIIIIIIIIIII
+/*
+class Tacka
+{
+    int x;
+    int y;
+public:
+    void Inic(int a,int b)
+    {
+        x = a;
+        y= b;
+    }
+
+    void Translacija(int dx,int dy)
+    {
+        x +=dx;
+        y +=dy;
+    }
+
+    void Pozicija()
+    {
+        cout<<"(x,y) = >"<<x<<","<<y<<endl;
+    }
+};
+
+
+
+int main()
+{
+        Tacka t;
+
+        t.Inic(5,6);
+        t.Pozicija();
+
+        t.Translacija(3,2);
+        t.Pozicija();
+}
+
+
+
+
+
+class Student
+{
+    char ime[10];
+    char prezime[20];
+    int index;
+public:
+
+    void Inic(char *i,char *p,int x)
+    {
+        strcpy(ime,i);
+        strcpy(prezime,p);
+        index = x;
+    }
+
+    void Ispis()
+    {
+        cout<<"Ime:"<<ime<<"\nPrezime:"<<prezime<<"\nBroj indeksa:"<<index<<endl;
+    }
+};
+
+int main()
+{
+    Student halil;
+
+    halil.Inic("Halil","Sacirovic",15);
+    halil.Ispis();
+}
+
+
+
+
+class Automobil
+{
+public:
+    double rezervoar;
+    int potrosnja;
+public:
+    void Inic(int gorivo,int p)
+    {
+        rezervoar = gorivo;
+        potrosnja = p;
+    }
+
+    double Predji(double km)
+    {
+        rezervoar -= (potrosnja*km/100.0);
+        cout<<"Potrosnja Automobila:"<<potrosnja*km/100.0<<endl;
+        return rezervoar;
+    }
+
+    void Ispis()
+    {
+        cout<<"Rezervoar:"<<rezervoar<<"\nPotrosnja:"<<potrosnja<<endl;
+    }
+};
+
+
+int main()
+{
+    Automobil audi;
+
+    audi.Inic(62,8);
+    audi.Ispis();
+
+    audi.Predji(50.5);
+    audi.Ispis();
+
+}
+
+
+
+class DvaZnaka
+{
+    char c1;
+    char c2;
+public:
+    void Inic(char z1,char z2)
+    {
+        this->c1=z1;
+        this->c2= z2;
+
+    }
+
+    void Uvecaj()
+    {
+        c1++;
+        c2++;
+    }
+    void Ispis()
+    {
+        cout<<"c1:"<<c1<<"\nc2:"<<c2<<endl;
+    }
+};
+
+
+int main()
+{
+    DvaZnaka dz;
+
+    dz.Inic('A','B');
+    dz.Ispis();
+
+    dz.Uvecaj();
+    dz.Ispis();
+}
+*/
+
+
+
