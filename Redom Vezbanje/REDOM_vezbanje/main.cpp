65c1b86e23c3f8589110279d40782f7b6835aa19
HalilSacirovic <halilsacirovic03@gmail.com>
2023-05-27 12:31:06 +0200

    Set i Get metoda

--- Redom Vezbanje/REDOM_vezbanje/main.cpp
+++ Redom Vezbanje/REDOM_vezbanje/main.cpp
@@ -272,7 +272,6 @@ int main() {
     Tacka2 teme2(4, 5);
     Tacka3 teme3(teme2, 6);
 }
-*/
 
 
 class Tacka
@@ -332,5 +331,318 @@ int main ()
     t2.Ispis();
     t2.Translacija(13,2);
     t2.Ispis();
+}
+
+*/
+
+
+// SET I GET METODE SA PRIVATNIM ATRIBUTIMA;
+
+/*
+
+
+class Clan
+{
+private:
+    int cbroj;
+    char imeprezime[30];
+    double stanje;
+    int pocasni;
+public:
+    Clan()
+    {
+        cbroj = 123;
+        strcpy(imeprezime,"Halil Sacirovic");
+        stanje = 5000;
+        pocasni = 0;
+    }
+
+    Clan(int cb,char *ip,double s,int p)
+    {
+        cbroj = cb;
+        strcpy(imeprezime,ip);
+        stanje = s;
+        pocasni = p;
+    }
+
+    void setBroj(int cb){cbroj = cb;}
+    void setIP(char *ip){strcpy(imeprezime,ip);}
+    void setStanje(double s){stanje = s;}
+    void setPocasni(int p){pocasni =p;}
+
+    int getCbroj(){return cbroj;}
+    char* getIP(){return imeprezime;}
+    double getStanje(){return stanje;}
+    int getPocasni(){return pocasni;}
+
+
+    void Naplata()
+    {
+        if(pocasni != 0)
+        {
+            stanje = stanje-500;
+        }
+    }
+};
+
+
+int main()
+{
+    Clan c1,c2;
+
+    c1.setBroj(5);
+    c1.Naplata();
+
+    c2.setBroj(6);
+    c2.setIP("Halil Sacirovic");
+    c2.setPocasni(5);
+    c2.setStanje(1600);
+    c2.Naplata();
+
+    cout<<"Stanje za Clana 1 posle naplate:"<<c1.getStanje()<<endl;
+
+    cout<<"Stanje za Clana 1:"<<c2.getIP()<<" posle naplate:"<<c2.getStanje()<<endl;
+    cout<<c2.getPocasni();
+}
+
+
+
+
+
+class Niz
+{
+public:
+    int *elementi;
+    int n;
+public:
+    Niz()
+    {
+        int i;
+        n=10;
+        elementi = new int[n];
+
+        for(i =0; i<n;i++)
+        {
+            elementi [i]=0;
+            cout<<"\nKonstruktor koji daje niz od 10 nula"<<endl;
+        }
+    }
+    Niz(int broj_el)
+    {
+        n = broj_el;
+        elementi = new int[n];
+    }
+
+    void Ucitaj_Elemente()
+    {
+        int i;
+        cout<<"Unesite elemente niza:";
+        for(i=0;i<n;i++)
+        {
+            cin>>elementi[i];
+        }
+    }
+
+    Niz(const Niz& niz1)
+    {
+        int i;
+        n = niz1.n;
+        elementi= new int[n];
+        for(i=0;i<n;i++)
+        {
+            elementi[i]=niz1.elementi[i];
+
+        }
+        cout<<"Kopy konstruktor"<<endl;
+    }
+    ~Niz()
+    {
+        delete [] elementi;
+    }
+
+    int Max()
+    {
+        int i;
+        int m= elementi[0];
+        for(i=0;i<n;i++)
+        {
+            if(m<elementi[i])
+            {
+                m = elementi[i];
+            }
+        }
+        return m;
+    }
+
+    void Ispis()
+    {
+        int i;
+        cout<<"Elementi niza su:";
+        for(i=0;i<n;i++)
+        {
+            cout<<elementi[i]<<" ";
+        }
+    }
+
+    int Srednja()
+    {
+        int i;
+        double suma=0;
+        double sr_vr;
+        for(i=0;i<n;i++)
+        {
+            suma += elementi[i];
+        }
+
+        sr_vr = suma/n;
+        return sr_vr;
+    }
+
+
+    Niz dodaj(const Niz& n2)
+    {
+        Niz n3(n);
+        int i;
+
+        for(i=0;i<n;i++)
+        {
+            n3.elementi[i]= elementi[i];
+
+        }
+        for(i=0;i<n2.n;i++)
+        {
+            n3.elementi[i] +=n2.elementi[i];
+        }
+
+        return n3;
+    }
+
+
+};
+
+
+int main()
+{
+    Niz n1,n2(5),n3(n2);
+
+
+
+    n1.Ispis();
+    cout<<endl;
+
+    n2.Ucitaj_Elemente();
+    n2.Ispis();
+    cout<<endl;
+
+
+    n3.Ucitaj_Elemente();
+    n3.Ispis();
+    cout<<endl;
+
+    cout<<"Najveci element niza 2:"<<n2.Max()<<endl;
+
+
+
+    cout<<endl;
+
+
+    cout<<"Sredja vrednost  niza 2:"<< n2.Srednja()<<endl;
+
+
+
+    cout<<endl;
+
+     cout<<"Sredja vrednost  niza 3:"<< n3.Srednja()<<endl;
+
+
+    cout<<endl;
+
+
+
+    cout<<"Najveci element niza 3:"<<n3.Max()<<endl;
+
+
+
+}
+*/
+
+
+class Polinom
+{
+    int n;
+    double *koeficijent;
+public:
+    Polinom(int n1)
+    {
+        n = n1;
+        koeficijent = new double[n];
+
+    }
+
+    Polinom()
+    {
+        n=0;
+        koeficijent = new double[n+1];
+        for(int i=0;i<n;i++)
+        {
+            koeficijent[i] = 1;
+        }
+    }
+
+    Polinom(const Polinom &poly)
+    {
+        n = poly.n;
+        koeficijent = new double[n+1];
+        for(int i=0;i<n+1;i++)
+        {
+            koeficijent[i] = poly.koeficijent[i];
+        }
+    }
+
+    void Ucitaj()
+    {
+     cout<<"Unesite koeficijente za polinom stepena "<<n<<endl;
+     for(int i=0;i<n+1;i++)
+         cin>>koeficijent[i];
+
+    }
+
+    void Ispis()
+    {
+        cout<<"p="<<koeficijent[0];
+        for(int i=1;i<n+1;i++)
+        cout<<" + "<<koeficijent[i]<<"x^"<<i;
+        cout<<endl;
+
+    }
+
+    int Izracunaj(int x)
+    {
+        int v_polinom = koeficijent[0];
+        int stepenx=1;
+        for(int i=1;i<n+1;i++)
+        {
+            stepenx *=x;
+        v_polinom  +=koeficijent[i]*stepenx;
+        }
+
+        return v_polinom;
+    }
+
+
+};
+
+int main(){
+int x;
+Polinom p,p1(3);
+p;
+p1.Ucitaj();
+Polinom p2=p1;
+p.Ispis();
+p1.Ispis();
+p2.Ispis();
+cout<<"Unesite x";
+cin>>x;
+cout<<"Vrednost polinom p2 u tacki x= "<<x<<"  je"<<p2.Izracunaj(x)<<endl;
 }
 
